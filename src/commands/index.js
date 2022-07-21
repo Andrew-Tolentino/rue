2a6fe696af4a7664a2c18fb2bc6020058e5cc142
atolentino-rt <atolentino@reviewtrackers.com>
2022-07-20 20:12:28 -0500

    Create delete command

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,4 +1,5 @@
 const AddCommand = require('./add.comand');
 const ReadCommand = require('./read.command');
+const DeleteCommand = require('./delete.command');
 
-module.exports = [AddCommand, ReadCommand];
+module.exports = [AddCommand, ReadCommand, DeleteCommand];
